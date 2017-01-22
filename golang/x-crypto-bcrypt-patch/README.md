0a45b79539fd7191c51fdbe74f03c521014fc8f3
Cruxic <cruxic@gmail.com>
2017-01-22 07:33:56 -0800

    patch to bcrypt which allows external salt

---
+++ golang/x-crypto-bcrypt-patch/README.md
@@ -0,0 +1,3 @@
+This is a patch to x/crypto/bcrypt which allows salt to be passed in instead of generated from a random source.
+
+Copy (or symlink) these files into your `$GOPATH/src/golang.org/x/crypto/bcrypt/` directory.  (Assuming you alread did `go get golang.org/x/crypto/bcrypt`)
---
+++ golang/x-crypto-bcrypt-patch/bcrypt_external_salt_patch.go
@@ -0,0 +1,44 @@
+package bcrypt
+
+import "errors"
+
+func newFromPasswordAndSalt(password, unencodedSalt []byte, cost int) (*hashed, error) {
+	if cost < MinCost {
+		cost = DefaultCost
+	}
+	p := new(hashed)
+	p.major = majorVersion
+	p.minor = minorVersion
+
+	err := checkCost(cost)
+	if err != nil {
+		return nil, err
+	}
+	p.cost = cost
+	
+	if len(unencodedSalt) != maxSaltSize {
+		return nil, errors.New("bcrypt: salt must be exactly 16 bytes")
+	}
+
+	/*unencodedSalt := make([]byte, maxSaltSize)
+	_, err = io.ReadFull(rand.Reader, unencodedSalt)
+	if err != nil {
+		return nil, err
+	}*/
+
+	p.salt = base64Encode(unencodedSalt)
+	hash, err := bcrypt(password, p.cost, p.salt)
+	if err != nil {
+		return nil, err
+	}
+	p.hash = hash
+	return p, err
+}
+
+func GenerateFromPasswordAndSalt(password, rawSalt []byte, cost int) ([]byte, error) {
+	p, err := newFromPasswordAndSalt(password, rawSalt, cost)
+	if err != nil {
+		return nil, err
+	}
+	return p.Hash(), nil
+}
