59a265ccbd84c0a70400e05a69de184d8a9ec057
Adam B <cruxic@gmail.com>
2017-01-27 07:28:48 -0800

    test unbiased_rand_int8

--- golang/calcpass/shared.go
+++ golang/calcpass/shared.go
@@ -2,8 +2,42 @@ package calcpass
 
 import (
 	"golang.org/x/crypto/bcrypt"
-	"fmt"
+	//"fmt"
+	//"encoding/hex"
+	"crypto/hmac"
+	"crypto/sha256"
+	"errors"
 )
+
+const NumBcryptThreads = 4
+const BcryptCost = 14
+
+func erase(sensitive []byte) {
+	for i := range sensitive {
+		sensitive[i] = 0
+	}
+}
+
+type thread_result struct {
+	threadIndex int	
+	bcryptHash []byte
+	err error
+}
+
+type byte_source interface {
+	NextByte() (byte, error)
+}
+
+//Constant salts for each thread (16 bytes each)
+func getThreadSalts() [NumBcryptThreads][]byte {
+	//there is nothing special about these constants - they were chosen randomly
+	return [NumBcryptThreads][]byte{
+		[]byte{0xa4,0xf8,0x01,0x6a,0x3f,0xc7,0x82,0xb2,0x1c,0xfa,0x2b,0xdf,0x55,0x9d,0x0b,0x2b},
+		[]byte{0xcc,0x9f,0x7a,0x24,0xc8,0x7b,0x85,0xf3,0x91,0x80,0x29,0xee,0x7f,0x09,0x44,0x9a},
+		[]byte{0x2a,0x34,0xbe,0xf1,0x9f,0xb4,0x0c,0xcd,0x69,0x7e,0xe9,0x70,0xb4,0x8e,0xab,0x15},
+		[]byte{0x33,0xfc,0xf3,0x30,0x9f,0x26,0xd2,0x68,0x75,0xf5,0x4d,0x10,0x5c,0x58,0xc4,0xfe},
+	}
+}
 
 /**
 Convert the plain-text card lock password into a 32-byte key.
@@ -16,16 +50,136 @@ encryption key and use that to encrypt the output of this function.  It
 should be stored WITHOUT an oracle which tells the attacker if the decryption
 succeeded or not, otherwise it completely circumvents the guessing resistance
 of this function.  The user must validate correct decryption with their "check word".
-*/
-func HashCardLockPassword(plaintext string) []byte {
-	return nil
 
+Returns nil if plaintext is zero length.  Caller is expected to
+ trim leading and trailing white space.
+*/
+func HashCardLockPassword(plaintextPassword []byte) ([]byte, error) {
+	if len(plaintextPassword) == 0 {
+		return nil, errors.New("empty password");
+	}
+	
+	//In a garbage collected language like Go (or Java, JavaScript, etc) we have no guarantee that
+	// any piece of memory is not going to be copied around.  The Go crypto designers
+	// went as far as removing cipher.Block.Reset() because it "is promising something 
+	//  that we can't deliver" (https://github.com/golang/go/issues/2841)
+	//
+	//That said, I'll make a best effort to avoid littering the heap with the plaintext.
+	
+	//Get away from the plaintext ASAP by hashing it
+	var sha = sha256.New()
+	sha.Write(plaintextPassword)
+	passwordShadow := sha.Sum(nil)
+	sha.Reset()
+	plaintextPassword = nil  //caller must still erase it
+	
+	salts := getThreadSalts()
+	
+	//Derive a different key for each bcrypt thread to work on
+	keys := [NumBcryptThreads][]byte{}
+	for i, salt := range salts {
+		keys[i] = hmacSha256(passwordShadow, salt)
+	}
+	
+	erase(passwordShadow)
+	
+	//Spawn bcrypt threads.
+	resultChan := make(chan *thread_result, NumBcryptThreads)
+	for i := range keys {
+		go bcryptThread(keys[i], salts[i], i, resultChan)
+	}
+
+	//Wait for threads to finish
+	var bcryptHashes [NumBcryptThreads][]byte
+	for _ = range keys {
+		res := <-resultChan
+		if res.err != nil {
+			//will not happen because key and salt are known-good length
+			return nil, res.err
+		}
+		
+		//sanity
+		if bcryptHashes[res.threadIndex] != nil {
+			panic("duplicate thread index")
+		}
+		
+		bcryptHashes[res.threadIndex] = res.bcryptHash
+	}
+	
+	//Digest the bcryptHashes sequentially
+	sha.Reset()
+	for i := range bcryptHashes {
+		sha.Write(bcryptHashes[i])
+	}
+	
+	finalHash := sha.Sum(nil)
+	
+	//Cleanup
+	sha.Reset()
+	for i := range keys {
+		erase(keys[i]);
+		erase(bcryptHashes[i]);
+	}
+	
+	//prevent erase getting optimized away
+	if (keys[0][0] + bcryptHashes[0][0] + passwordShadow[0]) != 0 {
+		panic("erase failed");
+	}
+	
+	return finalHash, nil
+}
+
+func bcryptThread(password, salt []byte, threadIndex int, result chan *thread_result) {
+	var tr thread_result
+	tr.threadIndex = threadIndex
+	tr.bcryptHash, tr.err = bcrypt.GenerateFromPasswordAndSalt(password, salt, BcryptCost)
+	
+	//remove salt and cost prefix (first 29 chars)
+	if tr.err == nil {
+		tr.bcryptHash = tr.bcryptHash[29:]
+	}
+
+	result <- &tr
 }
 
 
+func hmacSha256(key, message []byte) []byte {
+	hm := hmac.New(sha256.New, key)
+	hm.Write(message)
+	hash := hm.Sum(nil)
+	return hash
+}
 
-func What(i int) int {
-	res, _ := bcrypt.GenerateFromPasswordAndSalt([]byte("helloworld"), []byte("1234567890123456"), 5)	
-	fmt.Println(string(res))
-	return i + 1;
+/**Create a random integer from [0, n) where n is <= 256.
+This function returns uniformly distributed numbers (no modulo bias).
+
+I am not using math/rand because Intn() consumes bits too quickly (64 at a time).
+
+Returns error if the random source is exhausted or n exceeds 256.
+*/
+func unbiased_rand_int8(source byte_source, n int) (int, error) {
+	//Solution from:
+	//  https://zuttobenkyou.wordpress.com/2012/10/18/generating-random-numbers-without-modulo-bias/
+
+	var err error;
+	var b byte;
+	const randmax = 255
+	
+	if n > (randmax + 1) {
+		return -1, errors.New("unbiased_rand_int8: n too large");
+	}
+	
+	rand_limit := randmax - ((randmax+1) % n);
+	r := rand_limit + 1;
+	
+	for r > rand_limit {
+		b, err = source.NextByte();
+		if err != nil {
+			return -1, err;
+		}
+
+		r = int(b);
+	}
+
+	return r % n, nil;
 }
--- golang/calcpass/shared_test.go
+++ golang/calcpass/shared_test.go
@@ -3,7 +3,202 @@ package calcpass
 import (
 	"testing"
 	"encoding/hex"
+	"errors"
+	"fmt"
+	"crypto/sha256"
 )
+
+/*type fixed_rand_source struct {
+	index int
+	values []int64
+}
+
+func (self *fixed_rand_source) Int63() int64 {
+	v := self.values[self.index % len(self.values)];
+	self.index++;
+	return v;
+}
+
+func (self *fixed_rand_source) Seed(seed int64) {
+	//just reset index
+	//self.index = 0;
+}*/
+
+type fixed_byte_source struct {
+	index int
+	values []byte	
+}
+
+func (self *fixed_byte_source) NextByte() (byte, error) {
+	if self.index >= len(self.values) {
+		return 0, errors.New("END")
+	}
+	
+	v := self.values[self.index];
+	self.index++;
+	
+	return v, nil
+}
+
+/**Cycle through all byte values*/
+type cycle_byte_source struct {
+	nextVal uint
+	cycleCount int
+	maxCycleCount int
+}
+
+func (self *cycle_byte_source) reset() {
+	self.nextVal = 0
+	self.cycleCount = 0
+}
+
+func (self *cycle_byte_source) NextByte() (byte, error) {
+	if self.cycleCount > self.maxCycleCount {
+		return 0, errors.New("END")
+	}
+
+	b := byte(self.nextVal)
+	self.nextVal++
+
+	if self.nextVal > 0xFF {
+		self.cycleCount++
+		self.nextVal = 0
+	}
+
+	return b, nil
+}
+
+/*func read_all_unbiased_rand_int8(src byte_source, n int) []int {
+	var values []int;
+	for i := 0; i < 9999; i++ {
+		b, err := unbiased_rand_int8(src, n);
+		if err != nil {
+			return values;
+		}
+
+		values = append(values, b);		
+	}
+
+	return nil;
+}*/
+
+type rand_int8_func func (byte_source, int)(int,error)
+
+func hasGoodDistribution(src byte_source, fn rand_int8_func, n int) bool {
+	counts := make([]int, n)
+	
+	iterations := 3 * 256  //do at least 3 cycles of 0-255 from the byte_source
+	
+	for i := 0; i < iterations * 2; i++ {
+		b, err := fn(src, n)
+		if err != nil {
+			fmt.Println("Got err: ", err)
+			return false
+		}
+
+		counts[b]++
+		
+		//After we have done the target number of iterations check
+		// if the counts are uniformly distributed (identical counts).
+		if i > iterations {
+			uniform := true
+			for j := 1; j < n; j++ {
+				if counts[0] != counts[j] {
+					uniform = false
+					break
+				}			
+			}
+			if uniform {
+				//fmt.Printf("n=%d was uniform after %d iterations\n", n, i)
+				return true
+			}
+		}
+	}
+
+	//fmt.Printf("%d\t: ", n)
+	//fmt.Println(counts)
+
+	return false
+}
+
+
+func intSliceEq(a, b []int) bool {
+	if a == nil && b == nil { 
+		return true; 
+	}
+
+	if a == nil || b == nil { 
+		return false; 
+	}
+
+	if len(a) != len(b) {
+		return false
+	}
+
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+
+	return true
+}
+
+func bad_rand_int8(source byte_source, n int) (int, error) {
+	b, err := source.NextByte();
+	if err != nil {
+		return -1, err;
+	} else {
+		return int(b) % n, nil
+	}
+}
+
+func baseline_rand_int8(source byte_source, n int) (int, error) {
+	b, err := source.NextByte();
+	if err != nil {
+		return -1, err;
+	} else {
+		return int(b) % n, nil
+	}
+}
+
+func TestUnbiased_rand_int8(t *testing.T) {
+	//Test it against math/rand.Intn()
+
+	src := &cycle_byte_source{
+		maxCycleCount: 9999,		
+	}
+
+	//bad_rand_int8 suffers from modulo bias
+	if hasGoodDistribution(src, bad_rand_int8, 26) {
+		t.Error("hasGoodDistribution is broken")
+		return
+	}
+	
+	//Test unbiased_rand_int8 with all possible N
+	for n := 1; n <= 256; n++ {
+		src.reset()
+		if !hasGoodDistribution(src, unbiased_rand_int8, n) {
+			t.Errorf("unbiased_rand_int8 is non-uniform with n=%d", n)
+			return
+		}
+	}
+	
+	//n too large
+	src.reset()
+	_, err := unbiased_rand_int8(src, 257)
+	if err == nil {
+		t.Error("expected error but got none")
+	}
+	
+	//souce exhausted
+	src.reset()
+	src.cycleCount = src.maxCycleCount + 1
+	_, err = unbiased_rand_int8(src, 26)
+	if err == nil {
+		t.Error("expected error but got none")
+	}
+}
 
 //Make sure I'm using hmac and sha256 correctly
 func TestHmacSha256(t *testing.T) {
@@ -41,10 +236,59 @@ func TestHashCardLockPassword(t *testing.T) {
 	}
 	
 	got := hex.EncodeToString(hash);
-	expect := "9e0cc11fdb46698b33f24ff792cc6ec5a1572e5d5ad5a63c6441bcd41c0d0b8f";
+	expect := "df5c501779b0c3fb65a0c67386c561f625a5f622c3f2f31914ee463fa93d67d9";
 	if got != expect {
-		t.Error("Wrong hash: %s", got);
+		t.Error("Wrong hash: ", got);
 	}
 }
+
+//A condensed version of HashCardLockPassword which does not use threads for acceleration
+func singleCoreHashCardLockPassword(plaintextPassword []byte) ([]byte, error) {
+	var sha = sha256.New()
+	sha.Write(plaintextPassword)
+	passwordShadow := sha.Sum(nil)
+	
+	salts := getThreadSalts()
+	
+	keys := [NumBcryptThreads][]byte{}
+	for i, salt := range salts {
+		keys[i] = hmacSha256(passwordShadow, salt)
+	}
+	
+	sha.Reset()
+	resultChan := make(chan *thread_result, 1)
+	for i := range keys {
+		bcryptThread(keys[i], salts[i], i, resultChan)
+		res := <-resultChan
+		if res.err != nil {
+			return nil, res.err
+		}
+		if res.threadIndex != i {
+			return nil, errors.New("bad threadIndex")
+		}
+		sha.Write(res.bcryptHash)
+	}
+	
+	return sha.Sum(nil), nil
+}
+
+
+//Test for possible threading problems
+func TestHashCardLockPasswordSingleCore(t *testing.T) {
+	hash, err := singleCoreHashCardLockPassword([]byte("SuperSecretPassword"))
+	if err != nil {
+		t.Error(err);
+		return;
+	}
+	
+	got := hex.EncodeToString(hash);
+	//should have same output as TestHashCardLockPassword
+	expect := "df5c501779b0c3fb65a0c67386c561f625a5f622c3f2f31914ee463fa93d67d9";
+	if got != expect {
+		t.Error("Wrong hash: ", got);
+	}
+}
+
+
 
 //
