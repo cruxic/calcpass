efd018d1dd02212f5de93f01a3a0ed07ebef1ec7
Adam B <cruxic@gmail.com>
2017-06-23 12:53:10 -0700

    remove unused var

--- www/js/src/bcrypt.ts
+++ www/js/src/bcrypt.ts
@@ -102,17 +102,8 @@ export function rawBcrypt(pass: Uint8Array, salt: Uint8Array, cost:number): Uint
 /**Encode raw data using bcrypts flavor of Base64.*/
 export function encodeBcrypt64(data: Uint8Array): string {
 	//bcrypt's own non-standard base64 dictionary.
-    let BASE64_CODE = "./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split('');
+	let BASE64_CODE = "./ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789".split('');
 	
-    let BASE64_INDEX = [-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
-        -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0,
-        1, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, -1, -1, -1, -1, -1, -1,
-        -1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19,
-        20, 21, 22, 23, 24, 25, 26, 27, -1, -1, -1, -1, -1, -1, 28, 29, 30,
-        31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47,
-        48, 49, 50, 51, 52, 53, -1, -1, -1, -1, -1];
-
 	if (!data || data.length == 0)
 		throw new Error("Empty data");
 
